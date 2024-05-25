055f8b069235204ecc2f18a37702a1e82faf3068
BotFragger <m.ronikusuma@gmail.com>
2024-05-25 16:55:02 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Card from './components/Card'
+import Navigation from './components/Navigation'
 function App () {
   return (
     <>
-    <Card/>
+      <Navigation/>
     </>
   )
 }
