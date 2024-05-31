85cfca867b0c20fd3769642f00a3f2d133986ce5
BotFragger <m.ronikusuma@gmail.com>
2024-05-31 09:28:35 +0700

    add footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import LandingPage from '../components/LandingPage'
 
 const HomePage = () => {
   return (
     <>
       <LandingPage/>
+      <Footer/>
     </>
   )
 }
