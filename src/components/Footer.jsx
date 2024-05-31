c2ec2f3a8348eba5338c88aaeb1c675e4943d76e
BotFragger <m.ronikusuma@gmail.com>
2024-05-31 10:14:37 +0700

    add team & img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer () {
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <a href="" className="flex items-center">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BudgetWise</span>
               </a>
             </div>
@@ -25,10 +25,10 @@ export default function Footer () {
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Terms</h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-                    <a href="https://github.com/themesberg/flowbite" className="hover:underline text-[#F4D35E]">Privacy & Policy</a>
+                    <a href="" className="hover:underline text-[#F4D35E]">Privacy & Policy</a>
                   </li>
                   <li>
-                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline text-[#F4D35E]">Terms & Condition</a>
+                    <a href="" className="hover:underline text-[#F4D35E]">Terms & Condition</a>
                   </li>
                 </ul>
               </div>
@@ -44,7 +44,7 @@ export default function Footer () {
         </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">BudgetWise</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="" className="hover:underline">BudgetWise</a>. All Rights Reserved.
             </span>
             <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -53,7 +53,7 @@ export default function Footer () {
               </a>
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                 <FaInstagramSquare className='fill-[#F4D35E] text-3xl'/>
-                <span className="sr-only">Discord community</span>
+                <span className="sr-only">Instagram page</span>
               </a>
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                 <FaTwitter className='fill-[#F4D35E] text-3xl'/>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import LandingPage from '../components/LandingPage'
+import Team from '../components/Team'
 
 const HomePage = () => {
   return (
     <>
       <LandingPage/>
+      <Team/>
       <Footer/>
     </>
   )
