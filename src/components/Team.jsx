6890a53807f519f13faa07003ea218da2e84363d
Rifqi <rifqism88@gmail.com>
2024-06-02 20:49:52 +0700

    add images asset from supabase and fix some configuration

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { supabase } from '../utils/supabaseConfig'
+import { image } from '../utils/api'
 
 export default function Team () {
-  const { data } = supabase.storage.from('bucket').getPublicUrl('filePath.jpg')
-
   return (
     <>
     <div className='flex flex-wrap items-center justify-center gap-8 bg-TIM'>
@@ -12,35 +10,35 @@ export default function Team () {
         <div className='flex mx-auto justify-center items-center'>
           <div className="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl p-3 gap-6 mx-4">
             <figure className="flex justify-center items-center rounded-2xl">
-                <img src="https://placehold.co/300x300" alt="Card Preview" className="rounded-2xl "/>
+                <img src={image.avatar2} alt="Card Preview" className="rounded-2xl"/>
             </figure>
           </div>
         </div>
-          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>Placeholder</h1>
-          <h2 className='text-[#F4D35E] text-xl font-semibold drop-shadow-md'>Front-End Developer</h2>
-      </div>
-      {/* <div className='flex flex-col justify-center items-center h-screen'>
-        <div className='flex mx-auto justify-center items-center'>
-          <div className="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl p-3 gap-6 mx-4">
-            <figure className="flex justify-center items-center rounded-2xl">
-                <img src="https://placehold.co/300x300" alt="Card Preview" className="rounded-2xl "/>
-            </figure>
-          </div>
-        </div>
-          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>Placeholder</h1>
+          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>M. Roni Kusuma</h1>
           <h2 className='text-[#F4D35E] text-xl font-semibold drop-shadow-md'>Front-End Developer</h2>
       </div>
       <div className='flex flex-col justify-center items-center h-screen'>
         <div className='flex mx-auto justify-center items-center'>
           <div className="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl p-3 gap-6 mx-4">
             <figure className="flex justify-center items-center rounded-2xl">
-                <img src="https://placehold.co/300x300" alt="Card Preview" className="rounded-2xl "/>
+                <img src={image.avatar} alt="Card Preview" className="rounded-2xl "/>
             </figure>
           </div>
         </div>
-          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>Placeholder</h1>
+          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>Nyoman Semarajaya</h1>
           <h2 className='text-[#F4D35E] text-xl font-semibold drop-shadow-md'>Front-End Developer</h2>
-      </div> */}
+      </div>
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <div className='flex mx-auto justify-center items-center'>
+          <div className="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl p-3 gap-6 mx-4">
+            <figure className="flex justify-center items-center rounded-2xl">
+                <img src={image.avatar3} alt="Card Preview" className="rounded-2xl "/>
+            </figure>
+          </div>
+        </div>
+          <h1 className='py-2 text-white text-3xl font-semibold drop-shadow-md'>M. Rifqi Syamsul M.</h1>
+          <h2 className='text-[#F4D35E] text-xl font-semibold drop-shadow-md'>Front-End Developer</h2>
+      </div>
     </div>
     </>
   )
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,4 @@
-async function signUpNewUser () {
-  const { data, error } = await supabase.auth.signUp({
-    email: 'example@email.com',
-    password: 'example-password',
-    options: {
-      emailRedirectTo: 'https://example.com/welcome'
-    }
-  })
-}
-
-async function signInWithEmail () {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: 'example@email.com',
-    password: 'example-password'
-  })
-}
+import { supabase } from './supabaseConfig'
 
 export const HistoryData = async () => {
   try {
@@ -27,4 +12,19 @@ export const HistoryData = async () => {
     console.error('Failed to fetch history data:', error)
     return []
   }
+}
+
+export const image = {
+  avatar: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/nyoman-img.png',
+  avatar2: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/FB_IMG_1713200915872.jpg',
+  avatar3: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/profile.jpeg',
+  coins: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Coins.png',
+  hero_logo: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Home_logo.png',
+  line: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Line.png?t=2024-06-02T13%3A33%3A13.996Z',
+  line2: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Line2.png',
+  shape: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Line2.png',
+  shape2: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Shape2.png',
+  shape3: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Shape3.png',
+  shape4: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Shape4.png',
+  star: 'https://jakimybhtjdbdhptzkxw.supabase.co/storage/v1/object/public/Images/Star.png'
 }
