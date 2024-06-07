8edff4c4aa9218c9da7dd561dec6345b8323ec57
Rifqi <rifqism88@gmail.com>
2024-06-07 20:11:31 +0700

    update

--- src/components/TombolGenerate.jsx
+++ src/components/TombolGenerate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TombolGenerate () {
   return (
     <div>
-        <button className="my-6 block font-bold text-white rounded-lg px-4 py-1 bg-[#EE946B]">Generate</button>
+        <button className="my-6 block font-bold text-white rounded-lg px-6 py-3 bg-[#EE946B]">Generate</button>
     </div>
   )
 }
